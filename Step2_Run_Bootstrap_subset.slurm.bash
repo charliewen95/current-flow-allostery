695e80da12f198960b3143a740a2f71f53d7ac4c
charliewen95 <chenyun.wen@westernu.edu>
2022-04-01 16:42:35 -0700

    Step 2 update

--- Step2_Run_Bootstrap_subset.slurm.bash
+++ Step2_Run_Bootstrap_subset.slurm.bash
@@ -10,5 +10,27 @@ export PATH=/cm/shared/apps\:$PATH
 which python
 
 seqidList=`seq 193 1 226`
-parallel -j 12 "echo python gb_network/Bootstrap_Betweenness_KS.py -dbp cx26_GB_Network_Database/cx26_GB_Network.db -qs '* FROM Networks WHERE (Seqid_1={1})' -fgc 25 --outputDatabase cx26_GB_Network_Database/cx26_GB_Betweenness_Bootstrapped_KS.db-wbd --alphas .05 .1 .15 -v -vl 2 ;date > Bootstrap_Betweenness_KS.Seqid_1__{1}.log; python Bootstrap_Betweenness_KS.py -dbp cx26_GB_Network_Database/cx26_GB_Network.db -qs '* FROM Networks WHERE (Seqid_1={1})' -fgc 25 --outputDatabase cx26_GB_Network_Database/cx26_GB_Betweenness_Bootstrapped_KS.db --alphas .05 .1 .15 -v -vl 2 >> Bootstrap_Betweenness_KS.Seqid_1__{1}.log;date >> Bootstrap_Betweenness_KS.Seqid_1__{1}.log" ::: $seqidList
-echo "Done"
+
+parallel -j 12 "python <<- EOF
+import current_flow_allostery
+current_flow_allostery.bootstrap_betweenness(\
+'output_2/network_database_name.db',\
+'output_3/GB_Betweenness_Bootstrapped_KS.db',\
+'SELECT * FROM Networks WHERE (Seqid_1={1})',\
+None,\
+None,\
+None,\
+None,\
+[0.05, 0.1, 0.15],\
+False,\
+None,\
+False,\
+True,\
+25,\
+False,\
+True,\
+2\
+)
+EOF" ::: $seqidList
+
+
