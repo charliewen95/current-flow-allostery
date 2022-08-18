0681ff9f1fa634d15bf43e8524a7140879f5fa30
charliewen95 <chenyun.wen@westernu.edu>
2022-08-17 17:11:25 -0700

    Step4 direct run fixed

--- Step4_Run_Network_Pathing.slurm.bash
+++ Step4_Run_Network_Pathing.slurm.bash
@@ -10,11 +10,11 @@ export PATH=/cm/shared/apps\:$PATH
 which python
 
 
-sys='n14k2'
-rep="rep1 rep2"
+sys='wt2'
+rep="rep1"
 nproc=4
 echo "Starting pathing computations for $sys $rep using $nproc threads on `date`"
-parallel -j $nproc "echo python  current_flow_allostery/compute_pathing.py --inputPath output_3/GB_Betweenness_Bootstrapped_KS.db -q '*, Seqid_1 + 226 * ( Chain_1 - 1 ) AS Resid_1, Seqid_2 + ( Chain_2 - 1 ) * 226 AS Resid_2 FROM Networks WHERE system = \"{1}\" and rep = \"{2}\" and Frame = 1' --sourceNodeNames {3} --targetNodeNames {4} --weightFunction 'reciprocal_1.' 'reciprocal_.61' --weightColumns 'Betweenness' 'TOTAL' --NodeColumns 'Resid_1' 'Resid_2' --groupingColumns 'system' 'rep' 'Frame' --stoppingCriteria 'convergence_.001' --outputDatabase output_4/Pathing.db -v -vl 2 ; python  current_flow_allostery/compute_pathing.py --inputPath output_3/GB_Betweenness_Bootstrapped_KS.db -q '*, Seqid_1 + 226 * ( Chain_1 - 1 ) AS Resid_1, Seqid_2 + ( Chain_2 - 1 ) * 226 AS Resid_2 FROM Networks WHERE system = \"{1}\" and rep = \"{2}\"' --sourceNodeNames {3} --targetNodeNames {4} --weightFunction 'reciprocal_1.' 'reciprocal_.61' --weightColumns 'Betweenness' 'TOTAL' --NodeColumns 'Resid_1' 'Resid_2' --groupingColumns 'system' 'rep' 'Frame' --stoppingCriteria 'convergence_.001' --outputDatabase output_4/Pathing.db -v -vl 2  > pathingLogFiles/Compute_Pathing.{1}.{2}.source_{3}.target_{4}.log" ::: $sys ::: $rep ::: `seq --separator " " 14 226 $((6*226))` ::: `seq --separator " " 47 226 $((6*226))`
+parallel -j $nproc "echo python  current_flow_allostery/compute_pathing.py --inputPath output_2/GB_Network.db -q '*, Seqid_1 + 226 * ( Chain_1 - 1 ) AS Resid_1, Seqid_2 + ( Chain_2 - 1 ) * 226 AS Resid_2 FROM Networks WHERE system = \"{1}\" and rep = \"{2}\" and Frame = 1' --sourceNodeNames {3} --targetNodeNames {4} --weightFunction 'reciprocal_1.' 'reciprocal_.61' --weightColumns 'Betweenness' 'TOTAL' --NodeColumns 'Resid_1' 'Resid_2' --groupingColumns 'system' 'rep' 'Frame' --stoppingCriteria 'convergence_.001' --outputDatabase output_4/Pathing.db -v -vl 2 ; python  current_flow_allostery/compute_pathing.py --inputPath output_2/GB_Network.db -q '*, Seqid_1 + 226 * ( Chain_1 - 1 ) AS Resid_1, Seqid_2 + ( Chain_2 - 1 ) * 226 AS Resid_2 FROM Networks WHERE system = \"{1}\" and rep = \"{2}\"' --sourceNodeNames {3} --targetNodeNames {4} --weightFunction 'reciprocal_1.' 'reciprocal_.61' --weightColumns 'Betweenness' 'TOTAL' --NodeColumns 'Resid_1' 'Resid_2' --groupingColumns 'system' 'rep' 'Frame' --stoppingCriteria 'convergence_.001' --outputDatabase output_4/Pathing.db -v -vl 2  > pathingLogFiles/Compute_Pathing.{1}.{2}.source_{3}.target_{4}.log" ::: $sys ::: $rep ::: `seq --separator " " 14 226 $((6*226))` ::: `seq --separator " " 47 226 $((6*226))`
 
 echo "Done running pathing on `date`"
 
